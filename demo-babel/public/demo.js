52038daf26d3cb2af7f183de7e8ad58bcb180350
ZhengruiLu <lu.zhengr@northeastern.edu>
2022-11-06 15:49:03 -0800

    update for demo-babel complete

---
+++ demo-babel/public/demo.js
@@ -0,0 +1,6 @@
+"use strict";
+// const downgraded to var
+(function () {
+  var sound = 'hi';
+  console.log(sound);
+})();
---
+++ demo-todo/site.js
@@ -0,0 +1,123 @@
+"use strict";
+
+(function() {
+    //like enum
+    const PAGES = {
+        TODOS: 'todos',
+        CAT: 'cat',
+    }
+    //expended state
+    const state = {
+        todos: [
+            { task: 'Nap', done: false },
+            { task: 'Knock stuff down', done: true },
+            { task: 'Eat', done: false },
+        ],
+        page: PAGES.TODOS,
+    };
+    
+
+    const appEl = document.querySelector('#app');
+    
+    appEl.addEventListener('click', (event) => {
+        if(event.target.classList.contains('todo')) {
+            const index = event.target.dataset.index;
+            state.todos[index].done = !state.todos[index].done;
+            render();
+            return;
+        } 
+        
+        if (event.target.classList.contains('to-delete')) {
+            const index = event.target.dataset.index;
+            state.todos.splice(index, 1);
+            render();
+            return;
+        }
+
+        if (event.target.classList.contains('page')) {
+            state.page = event.target.dataset.target;
+            render();
+            return;
+        }
+        //show the index
+        // console.log('Click', event.target.dataset.index);
+
+        //show the tag name
+        // console.log('Click', event.target.nodeName);
+
+        //make it complete
+        // event.target.classList.toggle('complete');
+    });
+
+    // const addTaskFormEl = document.querySelector('.add-task');
+    
+    appEl.addEventListener('submit', (event) => {
+        // cancel the e if this e is cancelable
+        event.preventDefault();
+        // add new task
+        const taskEl = document.querySelector('.task');
+        const task = taskEl.value;
+        state.todos.push({task, done: false});
+        taskEl.value = '';
+        render();
+    });
+
+
+    //define a function called render
+    function render() {
+        if (state.page === PAGES.TODOS) {
+            renderTodos();
+            return;
+        }
+
+        if (state.page === PAGES.CAT) {
+            renderCat();
+            return;
+        }
+    }
+
+    function renderTodos() {
+        const listHtml = state.todos.map( (todo, index) => {
+            const doneClass = todo.done ? "complete" : "";
+            return `
+            <li>
+                <span class="todo ${doneClass}" data-index="${index}">${todo.task}</span>
+                <button data-index="${index}" type="button" class="to-delete">X</button> 
+            </li>
+            `;
+        }).join('');
+
+        const html = `
+            <ul class="todos">${listHtml}</ul>
+            <form class="add-task" action="" method="GET">
+                <label>
+                    New task
+                    <input class="task" name="task">
+                </label>
+                <button type="submit" class="add">Add</button>
+            </form>
+            <button type="button" class="page" data-target="cat">
+                Go to Cat
+            </button>
+        `
+
+        appEl.innerHTML = html;
+    }
+
+    function renderCat() {
+        appEl.innerHTML = `
+            <img
+            src="http://placekitten.com/300/300"
+            alt="a random cat"
+            >
+            <button type="button" class="page" data-target="todos">
+                Go to Todos
+            </button>
+        `
+    }
+
+    //call render
+    render();
+})();
+
+
